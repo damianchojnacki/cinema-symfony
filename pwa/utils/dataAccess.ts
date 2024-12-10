e2001c928430d5d5a7067289334f02b6a593afb0
Damian Chojnacki <daamian3@gmail.com>
2024-12-10 16:55:51 +0100

    debug: build

--- pwa/utils/dataAccess.ts
+++ pwa/utils/dataAccess.ts
@@ -63,7 +63,13 @@ export const fetch = async <TData>(
   const resp = await isomorphicFetch(ENTRYPOINT + id, init)
   if (resp.status === 204) return
 
-  const json = await resp.json() as TData & Item | SymfonyError
+  let json = {}
+
+  try {
+    json = await resp.json() as TData & Item | SymfonyError
+  } catch (e) {
+    console.error(await resp.text())
+  }
 
   if (resp.ok) {
     return {
